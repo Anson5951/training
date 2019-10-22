167b5e0e947a277e1eb476b7210f1d9914384135
user <user@10.100.0.162>
2019-10-22 15:09:18 +0800

    change database from local database to remote database

--- RestFulWebWithJDBC/src/main/java/com/example/dao/JdbcConnector.java
+++ RestFulWebWithJDBC/src/main/java/com/example/dao/JdbcConnector.java
@@ -4,23 +4,55 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JdbcConnector {
 
 	public Connection connection = null;
-
+	
 	public JdbcConnector() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/test?useTimezone=true&serverTimezone=GMT";
-			connection = DriverManager.getConnection(url, "root", "password");
-			System.out.println(connection.isValid(0));
+			String url = "jdbc:mysql://remotemysql.com:3306/DVph4FIxDe";
+			connection = DriverManager.getConnection(url, "DVph4FIxDe", "g2lCZx7Bo1");
+			System.out.println("connection is valid : " + connection.isValid(0));
 
 		} catch (ClassNotFoundException | SQLException e) {
 
 			e.printStackTrace();
 		}
 	}
+/*
+ * 	public Connection connection = null;
 
+	@Value("${remotedatabase.connect.hosts}")
+	private String hosts;
+	
+	@Value("${remotedatabase.connect.databasename}")
+	private String databaseName;
+	
+	@Value("${remotedatabase.connect.parameters}")
+	private String parameters;
+	
+	@Value("${remotedatabase.connect.username}")
+	private String username;
+	
+	@Value("${remotedatabase.connect.password}")
+	private String password;
+	
+
+	public JdbcConnector() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://" + hosts + "/" + databaseName + "?" + parameters;
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println(connection.isValid(0));
+
+ */
 	public Connection getConnection() {
 
 		return connection;
